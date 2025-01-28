8ad06622bd26dcbecad4d59878c52b1150e303ea
golibnarzullayev <gnarzullayev2000@gmail.com>
2025-01-28 13:45:22 +0500

    MOH-3672: Conflict resolve

--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare enum NotificationType {
-    release = "release",
-    integrationtest = "integration-test"
-}
-declare const sendTelegramNotification: () => Promise<void>;
-declare const getNotificationMessage: (notificationType: NotificationType, isSuccess: any) => string;
